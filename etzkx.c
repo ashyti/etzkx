b032ba1a0e2d8a22b8009b076a25f370aa0ba9a8
Andi Shyti <andi@etezian.org>
2013-06-14 15:53:37 +0200

    etzkx.c: add inclusion to module.h
    Signed-off-by: Andi Shyti <andi@etezian.org>

--- etzkx.c
+++ etzkx.c
@@ -18,6 +18,7 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/workqueue.h>
 #include <linux/i2c.h>
